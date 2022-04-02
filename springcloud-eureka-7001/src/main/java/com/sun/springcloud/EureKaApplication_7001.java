ab679b403a20cee50d6d658b256711d8fbebbf16
zangguoyang <zanggy@zhulong.com.cn>
2022-04-02 11:12:03 +0800

    7001加了哈哈哈

--- springcloud-eureka-7001/src/main/java/com/sun/springcloud/EureKaApplication_7001.java
+++ springcloud-eureka-7001/src/main/java/com/sun/springcloud/EureKaApplication_7001.java
@@ -3,6 +3,7 @@ package com.sun.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//hahahha
 /*访问地址 localhost:7001*/
 @SpringBootApplication
 @EnableEurekaServer  /*服务端启动类*/
